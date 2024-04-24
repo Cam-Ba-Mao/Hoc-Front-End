54f6a1d82fd8e357e1042090e3394cdaa142a962
Ba Mao <maocam@rtholdings.edu.vn>
2024-04-24 16:36:35 +0700

    update css grid

---
+++ javascript.js
@@ -0,0 +1,9 @@
+document.getElementById("demo").innerHTML //thay text
+document.getElementById('myImage').src='pic_bulbon.gif' // thay src
+document.getElementById('demo').style.fontSize='35px' // css
+document.getElementById("demo").style.display = "none";
+
+// Writing into an HTML element, using innerHTML.
+// Writing into the HTML output using document.write().
+// Writing into an alert box, using window.alert().
+// Writing into the browser console, using console.log().
